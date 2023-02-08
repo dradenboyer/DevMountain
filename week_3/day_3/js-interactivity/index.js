34a42b761ee681efe13ce97a413770823c219add
Draden Boyer <dradenboyer@gmail.com>
2023-02-08 14:07:25 -0700

    incorporating javascript with html&css

---
+++ week_3/day_3/js-interactivity/index.js
@@ -0,0 +1,42 @@
+console.log('Hello world')
+const message = document.querySelector('#message')
+
+const addMovie = evt => {
+    evt.preventDefault();
+    const inputField = document.querySelector('input')
+    const movie = document.createElement('li')
+    const movieTitle = document.createElement('span')
+    movieTitle.addEventListener('click', crossOffMovie)
+    
+
+
+
+    movieTitle.textContent = inputField.value
+    movie.appendChild(movieTitle)
+    const deleteBtn = document.createElement('button')
+    deleteBtn.textContent = 'x'
+    deleteBtn.addEventListener('click', deleteMovie)
+    movie.appendChild(deleteBtn)
+
+    document.querySelector('ul').appendChild(movie)
+    inputField.value = ''
+}
+
+const deleteMovie = evt => {
+    evt.target.parentNode.remove()
+    message.textContent = 'Movie Deleted!'
+}
+
+const crossOffMovie = evt => {
+    evt.target.classList.toggle('checked')
+    if(evt.target.classList.contains('checked') === true){
+        message.textContent = 'Movie Watched!'
+    }else{
+        message.textContent = 'Movie added back!'
+    }
+}
+
+
+
+document.querySelector('form').addEventListener('submit', addMovie);
+console.log(addMovie)
